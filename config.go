187ba64c2db15cf87210b1dab55cea22f4e71cdc
d.shahmatov <dmitry.shahmatov@cyberok.ru>
2025-01-03 21:50:04 +0300

    Add config

--- config.go
+++ config.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	Keys struct {
+		VulnersApiKey string `json:"vulners_api_key"`
+	} `json:"keys"`
+	Threads struct {
+		PortScanner int `json:"port_scanner"`
+		Network     int `json:"network"`
+	} `json:"threads"`
+}
+
+func (c *Config) Read(path string) error {
+	configFile, err := os.Open(path)
+	if err != nil {
+		return fmt.Errorf("failed to read Сonfig: %v", err)
+	}
+	defer configFile.Close()
+
+	jsonParser := json.NewDecoder(configFile)
+	if err = jsonParser.Decode(c); err != nil {
+		return fmt.Errorf("failed to parse Сonfig: %v", err)
+	}
+	return nil
+}
