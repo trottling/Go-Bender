8a8b538765f7d0cfc2e7b6dae9ae4b45ecd48062
d.shahmatov <dmitry.shahmatov@cyberok.ru>
2025-01-03 21:50:56 +0300

    Add scanner initial funcs

--- scanner.go
+++ scanner.go
@@ -1,1 +1,37 @@
 package main
+
+import log "github.com/sirupsen/logrus"
+
+type Scanner struct {
+	config Config
+	args   Args
+	log    *log.Logger
+}
+
+func (scanner *Scanner) Init() error {
+	var err error
+
+	// Logger
+	scanner.log = GetLogger()
+
+	// Run arguments
+	scanner.args = Args{}
+	if err = scanner.args.Get(); err != nil {
+		return err
+	}
+
+	// Json config from file
+	scanner.config = Config{}
+	if err = scanner.config.Read(scanner.args.ConfigPath); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (scanner *Scanner) Scan() {
+	// Initial scanner
+	if err := scanner.Init(); err != nil {
+		scanner.log.Fatal(err)
+	}
+}
