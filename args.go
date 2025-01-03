adac1d58a8371f5eaa50f3c4059af9701fc8a5ab
d.shahmatov <dmitry.shahmatov@cyberok.ru>
2025-01-03 21:49:52 +0300

    Add args

---
+++ args.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/alexflint/go-arg"
+)
+
+type Args struct {
+	ConfigPath string `arg:"required,-c,--config" help:"config file path"`
+}
+
+func (args *Args) Get() error {
+	if err := arg.Parse(&args); err != nil {
+		return fmt.Errorf("arguments parse error: %w", err)
+	}
+	return nil
+}
